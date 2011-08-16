35efa6daff985edb67a93a64c0bb3a9adafc6b88
Evil.Ghost <lixiayi@verycd.com>
2011-08-16 13:49:59 +0800

    将widget工程转为静态库工程

--- test/main.cpp
+++ test/main.cpp
@@ -1,66 +1,9 @@
 
-#include "../utility/Utility.h"
-
-class TestClass 
-	: ghost::utility::NoCopyable
-	, public ghost::utility::Cloneable
-	, public ghost::utility::Associateable
-{
-	TestClass* m_pAnother;
-
-public:
-	TestClass()
-		: m_pAnother(0)
-	{
-
-	}
-	virtual ~TestClass()
-	{
-		DisassociateFromAll();
-	}
-
-public:
-	virtual TestClass* Clone()
-	{
-		return 0;
-	}
-
-public:
-	virtual bool AllowAssociatingWidth(Associateable* pObj) const
-	{
-		return 0 != dynamic_cast<TestClass*>(pObj);
-	}
-	virtual void DisassociateFromAll()
-	{
-		DisassociateFrom(m_pAnother);
-	}
-
-private:
-	virtual bool IsAssociatedWidth(Associateable* pObj) const
-	{
-		return dynamic_cast<TestClass*>(pObj) == m_pAnother;
-	}
-
-	virtual void DoAssociateWith(Associateable* pObj)
-	{
-		m_pAnother = dynamic_cast<TestClass*>(pObj);
-	}
-	virtual void DoDisassociateFrom(Associateable* pObj)
-	{
-		m_pAnother = 0;
-	}
-};
+#include "../widget/Widget.h"
 
 void Test()
 {
-	TestClass test1;
-	//TestClass test2(test1); // illegal
-	TestClass test3;
-	//test3 = test1; // illegal
-
-	TestClass* pTest1 = new TestClass();
-	test1.AssociateWith(pTest1);
-	delete pTest1;
+	
 }
 
 int main()
---
+++ widget/Widget.h
@@ -0,0 +1,28 @@
+
+#ifndef WIDGET_H
+#define WIDGET_H
+
+#include "WidgetContainer.h"
+#include "WidgetView.h"
+
+#ifdef _DEBUG
+
+#ifdef _DLL // /MDd
+#pragma comment(lib, "widget_MDd")
+
+#else // /MD
+#pragma comment(lib, "widget_MD")
+#endif // _DLL
+
+#else
+
+#ifdef _DLL // /MTd
+#pragma comment(lib, "widget_MTd")
+
+#else // /MT
+#pragma comment(lib, "widget_MT")
+#endif // _DLL
+
+#endif // _DEBUG
+
+#endif // WIDGET_H
